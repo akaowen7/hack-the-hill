9bf70a1bf6b7db67aa22d5d6a4ad4934bae0e50e
Kiera <kierahutton1@gmail.com>
2024-09-28 11:20:42 -0400

    Ant libary

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,10 +1,15 @@
 import Image from "next/image";
 import Link from "next/link";
 import Layout from "../components/layout";
+import {Card} from "antd";
+
 export default function Home() {
   return (
     <Layout> 
       <p>Hello</p>
+      <Card title="Card" >
+      <p>Card content</p>
+      </Card>
       <Link href="/about">About</Link>
     </Layout>
   );
