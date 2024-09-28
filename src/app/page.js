4f169442ca93d4e075d365feffcab0760120c958
Kiera <kierahutton1@gmail.com>
2024-09-28 14:27:04 -0400

    Home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,44 @@
 import { Card, Button, Progress } from "antd";
 
 export default function Home() {
-  return (
-    <Card title="Goal Name">
-      <div className="flex flex-col gap-4">
-        <p>[calendar/longer term progress]</p>
-        <Progress
-          type="line"
-          percent={50}
-          steps={3}
-          showInfo={false}
-        ></Progress>
+  const goals = [
+    {
+      name: "Goal 1",
+      todayProgress: 3,
+      totalProgress: 5,
+      completionInterval: 1,
+    },
+    {
+      name: "Goal 2",
+      todayProgress: 1,
+      totalProgress: 4,
+      completionInterval: 2,
+    },
+  ];
 
-        <Button type="primary">Log</Button>
-      </div>
-    </Card>
-  );
+  return goals.map((goal) => {
+    const { name, todayProgress, totalProgress, completionInterval } = goal;
+    return (
+      <Card title={name}>
+        <div className="flex flex-col gap-4">
+          <p>[calendar/longer term progress]</p>
+          <div>
+            <p>
+              {completionInterval === 1
+                ? "Today's progress: "
+                : "This week's progress:"}
+            </p>
+            <Progress
+              type="line"
+              percent={(100 * todayProgress) / totalProgress}
+              steps={totalProgress}
+              showInfo={false}
+            ></Progress>
+          </div>
+
+          <Button type="primary">Log</Button>
+        </div>
+      </Card>
+    );
+  });
 }
