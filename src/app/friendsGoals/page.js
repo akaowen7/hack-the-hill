e3bbd1b1faf67c0f2001309c4c3fb45fb6e256ca
Cameron Harrison <54902084+cambo602@users.noreply.github.com>
2024-09-28 17:43:23 -0400

    stuff :/

--- src/app/friendsGoals/page.js
+++ src/app/friendsGoals/page.js
@@ -1,9 +1,43 @@
+import { Card, Button, Progress } from "antd";
+import PegBoard from "../components/pegBoard/pegBoard";
 import Layout from "../components/layout";
 
-export default function FriendsGoals() {
+export default async function Home() {
+  const res = await fetch("http://localhost:3000/api/goals?userId=-1");
+  const goals = (await res.json()).rows;
+
+  console.log(goals);
   return (
     <Layout>
-      <p>Hello FriendsGoals</p>
+      {goals.map((goal) => {
+        const { name, todayprogress, totalprogress, frqeuncytype, completed } =
+          goal;
+        return (
+          <Card title={name}>
+            <div className="flex flex-col gap-4">
+              <PegBoard
+                num={completed}
+                type={frqeuncytype === 1 ? "Dots" : "Pills"}
+              />
+              <div>
+                <p>
+                  {frqeuncytype === 1
+                    ? "Today's progress: "
+                    : "This week's progress:"}
+                </p>
+                <Progress
+                  type="line"
+                  percent={(100 * todayprogress) / totalprogress}
+                  steps={totalprogress}
+                  showInfo={false}
+                ></Progress>
+              </div>
+
+              <Button type="primary">Log</Button>
+            </div>
+          </Card>
+        );
+      })}
     </Layout>
   );
 }
