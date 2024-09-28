5a5d2ce515c7c32ee50b8acb8e463dd93c821f01
Cameron Harrison <54902084+cambo602@users.noreply.github.com>
2024-09-28 15:44:43 -0400

    progress pegboard on cards

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import { Card, Button, Progress } from "antd";
+import PegBoard from "./components/pegBoard/pegBoard";
 
 export default function Home() {
   const goals = [
@@ -6,25 +7,31 @@ export default function Home() {
       name: "Goal 1",
       todayProgress: 3,
       totalProgress: 5,
-      completionInterval: 1,
+      frqeuncyInterval: 1,
+      completed: 1532349930,
     },
     {
       name: "Goal 2",
       todayProgress: 1,
       totalProgress: 4,
-      completionInterval: 2,
+      frqeuncyInterval: 2,
+      completed: 409,
     },
   ];
 
   return goals.map((goal) => {
-    const { name, todayProgress, totalProgress, completionInterval } = goal;
+    const { name, todayProgress, totalProgress, frqeuncyInterval, completed } =
+      goal;
     return (
       <Card title={name}>
         <div className="flex flex-col gap-4">
-          <p>[calendar/longer term progress]</p>
+          <PegBoard
+            num={completed}
+            type={frqeuncyInterval === 1 ? "Dots" : "Pills"}
+          />
           <div>
             <p>
-              {completionInterval === 1
+              {frqeuncyInterval === 1
                 ? "Today's progress: "
                 : "This week's progress:"}
             </p>
