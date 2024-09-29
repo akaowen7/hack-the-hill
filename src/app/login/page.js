94b8790ad62a5d4f1a17664053227b35eb9220c8
Reid Tull <reid.a.tull@gmail.com>
2024-09-28 21:55:04 -0400

    login

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,17 +1,40 @@
 "use client";
 
 import { Input, Button } from "antd";
-import React from "react";
+import React, { useState } from "react";
+import Cookies from "js-cookie";
+import { useRouter } from "next/navigation";
 
 export default function Login() {
+  const router = useRouter();
+  const [username, setUsername] = useState("");
+
   const handleSubmit = async (event) => {
-    console.log("Login");
+    if (!username) {
+      alert("Username is required");
+      return;
+    }
+    // send request to get login endpoint
+    const response = await fetch(`/api/login?username=${username}`);
+    const userId = (await response.json()).user.id;
+
+    console.log(userId);
+
+    // set user id in cookies
+    Cookies.set("userId", userId);
+
+    // redirect to home page
+    router.push("/");
   };
 
   return (
     <div className="h-screen w-screen flex justify-center">
       <div className="flex flex-col gap-4 my-auto">
-        <Input placeholder="Username" />
+        <Input
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
         <Button type="primary" onClick={handleSubmit}>
           Login / Sign Up
         </Button>
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,21 @@
 import Layout from "./components/layout";
 import GoalDisplayCard from "./components/goalDisplayCard";
+import { redirect } from "next/navigation";
+import { cookies } from "next/headers";
 
 export default async function Home() {
-  const res = await fetch("http://localhost:3000/api/goals?userId=-1");
+  // get user id cookie
+  const cookieStore = cookies();
+  const userIdCookie = cookieStore.get("userId");
+  if (!userIdCookie) {
+    console.log("User not logged in");
+    // redirect to login page
+    redirect("/login");
+  }
+
+  const res = await fetch(
+    `http://localhost:3000/api/goals?userId=${userIdCookie.value}`
+  );
   const goals = (await res.json()).rows;
   console.log(goals);
   return (
