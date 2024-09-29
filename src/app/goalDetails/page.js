2878a812d016ef08b90051e68b92afdaa49b0ba1
Max Zimmermann <hobomojo3@gmail.com>
2024-09-28 22:49:41 -0400

    Started filling in the details page

--- src/app/goalDetails/page.js
+++ src/app/goalDetails/page.js
@@ -6,16 +6,63 @@ import { useSearchParams } from "next/navigation";
 export default async function GoalDetails() {
   // Get the goalID from the URL
   const searchParams = useSearchParams();
-  const id = searchParams.get("id");
+  const res = await fetch(
+    "http://localhost:3000/api/goal?id=" + searchParams.get("id")
+  );
+  const goal = (await res.json()).rows;
 
-  const res = await fetch("http://localhost:3000/api/goal?id=" + id);
-
-  console.log(res);
-
+  const {
+    name,
+    todayProgress,
+    totalProgress,
+    frequencyInterval,
+    completed,
+    defaultIncrement,
+    id,
+  } = goal;
   return (
-    <Layout>
-      <p>goal details {id}</p>
-      <button>Log to console</button>
-    </Layout>
+    <Card title={name}>
+      <div className="flex flex-col gap-4">
+        <p>{isLoggingOpen}</p>
+        <PegBoard
+          num={completed}
+          type={frequencyInterval === 1 ? "Dots" : "Pills"}
+        />
+        <div>
+          <p>
+            {frequencyInterval === 1
+              ? "Today's progress: "
+              : "This week's progress:"}
+          </p>
+          <Progress
+            type="line"
+            percent={(100 * todayProgress) / totalProgress}
+            steps={totalProgress}
+            showInfo={false}
+          ></Progress>
+        </div>
+
+        <Button type="primary" onClick={() => setIsLoggingOpen(true)}>
+          Log
+        </Button>
+
+        <Button type="primary" onClick={() => sendToDetails(id)}>
+          View Details
+        </Button>
+
+        <Modal
+          title={name}
+          open={isLoggingOpen}
+          onCancel={() => setIsLoggingOpen(false)}
+        >
+          <p>Increment by </p>
+          <InputNumber
+            min={0}
+            max={defaultIncrement * 10}
+            defaultValue={defaultIncrement}
+          />
+        </Modal>
+      </div>
+    </Card>
   );
 }
